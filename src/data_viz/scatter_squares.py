5b0af223e8cb6793a9cd2dae0cdc93e36c2ccaf1
Nabil Khalil <nabilamirkhalil@gmail.com>
2024-04-08 10:50:45 -0400

    use a color map

--- src/data_viz/scatter_squares.py
+++ src/data_viz/scatter_squares.py
@@ -7,7 +7,7 @@ x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10, color="green")
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Blues)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -20,5 +20,8 @@ ax.ticklabel_format(style="plain")
 
 # Set size of tick labels.
 ax.tick_params(axis="both", labelsize=10)
+
+# Uncomment the line below to save the plot to a file.
+# plt.savefig("squares_plot.png", bbox_inches="tight")
 
 plt.show()
