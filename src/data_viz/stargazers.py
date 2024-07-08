c442a619220f813a9a4a6339afbfbdab3a0160ab
nkhalil98 <nabilamirkhalil@gmail.com>
2024-07-07 21:25:36 -0400

    rename

--- src/data_viz/stargazers.py
+++ src/data_viz/stargazers.py
@@ -1,16 +1,21 @@
 import requests
 import plotly.express as px
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 API_URL = "https://api.github.com/search/repositories"
 
 query = API_URL + "?q=language:python+sort:stars+stars:>10000"
 headers = {"Accept": "application/vnd.github.v3+json"}
 response = requests.get(query, headers=headers)
+response.raise_for_status()
 
 data = response.json()
 n_repos = data["total_count"]
 complete_results = not data["incomplete_results"]
-print(
+logger.info(
     f"Queried {n_repos} Python repositories with more than 10,000 stars. Complete results status: {complete_results}."
 )
 
@@ -20,11 +25,11 @@ links, stars, hover_texts = [], [], []
 for repo in available_repos:
     name = repo["name"]
     url = repo["html_url"]
-    link = f'<a href="{url}">{name}</a>'
+    link = f"<a href='{url}'>{name}</a>"
     star_count = repo["stargazers_count"]
     owner = repo["owner"]["login"]
     description = repo["description"]
-    hover_text = f"{owner}<br />{description}<br />{url}"
+    hover_text = f"{owner}<br />{description}"
     links.append(link)
     stars.append(star_count)
     hover_texts.append(hover_text)
@@ -34,7 +39,7 @@ fig = px.bar(
     x=links,
     y=stars,
     labels={"x": "Repository", "y": "Stars"},
-    title="Most popular Python repositories on GitHub",
+    title="Most starred Python repositories on GitHub",
     hover_name=hover_texts,
 )
 fig.update_layout(
